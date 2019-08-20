064568bd3c694edfd5a80da098c1c32c3030fd99
nrayamajhee <nishan@rayamajhee.com>
2019-08-20 13:32:00 -0500

    Shell script to install packages

--- install.sh
+++ install.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
+YAY=$(pacman -Qsq yay)
 
 case $1 in
 	"install" | "i")
+		sudo pacman -S --needed --noconfirm git base-devel
+		if [ "$YAY" == "" ]; then
+			echo "Yay not installed on system. Installing it!"
+			git clone https://aur.archlinux.org/yay-bin.git
+			cd yay-bin
+			makepkg -sri --noconfirm
+			cd ../
+			echo "Done installing yay"
+		fi
+		echo "Installing vim, tmux, powerline and airline"
+		yay -S --needed --noconfirm vim tmux powerline powerline-fonts-git vim-airline
+		echo "Installing Vundle"
+		 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+		vim +PluginInstall +qall
+		echo "Installed everything! run ./install apply to apply configurations"
+	;;
+	"apply" | "a")
 		mv ~/.vimrc ./vimrc.old
 		mv ~/.tmux.conf ./tmux.conf.old
 		cp ./vimrc ~/.vimrc
 		cp ./tmux.conf ~/.tmux.conf
 	;;
-	"uninstall" | "u")
+	"reset" | "r")
 		mv ./vimrc.old ~/.vimrc
 		mv ./tmux.conf.old ~/.tmux.conf
 	;;
 	*)
-		echo "install [(i)nstall | (u)ninstall]"
+		echo "install [(i)nstall | (a)ppy | (r)eset ]"
 	;;
 esac
