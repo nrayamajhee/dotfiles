1b9dab04934c9d01b52cfa5c91b569b808860437
nrayamajhee <nishan@rayamajhee.com>
2022-07-31 19:03:46 -0700

    Fix formatter

--- lua/load_plugins.lua
+++ lua/load_plugins.lua
@@ -1,24 +1,51 @@
-require('nvim-tree').setup()
-require('formatter').setup {
-  ['*'] = {
-    require('formatter.filetypes.any').remove_trailing_whitespace
-  }
-}
-require('ayu').colorscheme()
-require('lualine').setup {
-  options = {
-    theme = 'ayu_dark',
-    component_separators = { left = '|', right = '|' },
-    section_separators = { left = '', right = '' }
-  }
-}
-require('nvim-reload')
-require('bufferline').setup {
-  options = {
-    right_mouse_command = '',
-    middle_mouse_command = 'bdelete! %d',
-    show_close_icon = false,
-  }
-}
-require('Comment').setup()
-require('vgit').setup()
+require("nvim-tree").setup()
+require("formatter").setup({
+	filetype = {
+		html = {
+			require("formatter.defaults.prettier"),
+		},
+		css = {
+			require("formatter.defaults.prettier"),
+		},
+		javascript = {
+			require("formatter.defaults.prettier"),
+		},
+		javascriptreact = {
+			require("formatter.defaults.prettier"),
+		},
+		typescript = {
+			require("formatter.defaults.prettier"),
+		},
+		typescriptreact = {
+			require("formatter.defaults.prettier"),
+		},
+		rust = {
+			require("formatter.filetypes.rust").rustfmt,
+		},
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+	},
+})
+require("ayu").colorscheme()
+require("lualine").setup({
+	options = {
+		theme = "ayu_dark",
+		component_separators = { left = "|", right = "|" },
+		section_separators = { left = "", right = "" },
+	},
+})
+require("nvim-reload")
+require("bufferline").setup({
+	options = {
+		right_mouse_command = "",
+		middle_mouse_command = "bdelete! %d",
+		show_close_icon = false,
+	},
+})
+require("Comment").setup({
+	opleader = {
+		line = "<C-_>",
+	},
+})
+require("vgit").setup()
--- lua/vim_config.lua
+++ lua/vim_config.lua
@@ -1,8 +1,8 @@
 vim.opt.number = true
 vim.opt.spell = true
-vim.opt.spelllang = 'en_us' 
-vim.opt.mouse = 'a'
-vim.g.mapleader = ' '
+vim.opt.spelllang = "en_us"
+vim.opt.mouse = "a"
+vim.g.mapleader = " "
 vim.opt.termguicolors = true
 vim.opt.timeoutlen = 1000
 vim.opt.expandtab = true
@@ -13,25 +13,25 @@ vim.opt.shiftwidth = 2
 vim.opt.clipboard = unnamedplus
 
 vim.opt.relativenumber = true
-vim.api.nvim_create_augroup('numtoggle', {})
-vim.api.nvim_create_autocmd({'FocusGained','InsertLeave'}, {
-  group = 'numtoggle',
-  pattern = '*',
-  command = "set relativenumber"
+vim.api.nvim_create_augroup("numtoggle", {})
+vim.api.nvim_create_autocmd({ "FocusGained", "InsertLeave" }, {
+	group = "numtoggle",
+	pattern = "*",
+	command = "set relativenumber",
 })
-vim.api.nvim_create_autocmd({'FocusLost','InsertEnter'}, {
-  group = 'numtoggle',
-  pattern = '*',
-  command = "set norelativenumber"
+vim.api.nvim_create_autocmd({ "FocusLost", "InsertEnter" }, {
+	group = "numtoggle",
+	pattern = "*",
+	command = "set norelativenumber",
 })
 vim.api.nvim_set_hl(0, "Normal", {
-  bg = "none"
+	bg = "none",
 })
 vim.api.nvim_set_hl(0, "LineNr", {
-  bg = "none"
+	bg = "none",
 })
 --loaded_netrwPlugin  = false
 --vim.api.nvim_create_autocmd('FileType', {
-  --pattern = 'netrw',
-  --command = "setl bufhidden=wipe"
+--pattern = 'netrw',
+--command = "setl bufhidden=wipe"
 --})
