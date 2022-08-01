25602daa8125f60d282a6a72059f2feb60061dbc
nrayamajhee <nishan@rayamajhee.com>
2022-08-01 11:01:06 -0700

    Add metals as scala lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -31,6 +31,10 @@ require("lspconfig")["tsserver"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
 })
+require("lspconfig")["metals"].setup({
+	on_attach = on_attach,
+	flags = lsp_flags,
+})
 require("lspconfig")["rust_analyzer"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
