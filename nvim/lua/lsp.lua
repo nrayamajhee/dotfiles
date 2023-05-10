4d19607cf099d4d709f43a44579feaa1f91576db
nrayamajhee <nishan@rayamajhee.com>
2023-05-09 17:12:54 -0700

    Add tab support for cmp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -24,16 +24,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
 		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 		local opts = { buffer = ev.buf }
-    --vital
+		--vital
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    -- let trouble hande it
+		-- let trouble hande it
 		-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 		vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, opts)
 		vim.keymap.set({ "n", "v" }, "<leader>cc", vim.lsp.buf.code_action, opts)
-		vim.keymap.set('n', '<leader>ss', vim.lsp.buf.signature_help, opts)
-		vim.keymap.set('n', '<leader>kk', vim.lsp.buf.hover, opts)
-    
-    --vastigial
+		vim.keymap.set("n", "<leader>ss", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<leader>kk", vim.lsp.buf.hover, opts)
+
+		--vastigial
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 
@@ -67,6 +67,27 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Set `select` to `false` to only confirm explicitly selected items.
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
@@ -85,34 +106,21 @@ end
 --trouble
 
 require("trouble").setup()
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleClose<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>dw", "<cmd>Trouble workspace_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>dd", "<cmd>Trouble document_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>qf", "<cmd>Trouble quickfix<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "gr", "<cmd>TroubleToggle lsp_references<cr>",
-  {silent = true, noremap = true}
-)
-
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleClose<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>dw", "<cmd>Trouble workspace_diagnostics<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>dd", "<cmd>Trouble document_diagnostics<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>qf", "<cmd>Trouble quickfix<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "gr", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
 
 --metals
 local metals_config = require("metals").bare_config()
 metals_config.capabilities = capabilities
 local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "scala", "sbt" },
-  callback = function()
-    require("metals").initialize_or_attach(metals_config)
-  end,
-  group = nvim_metals_group,
+	pattern = { "scala", "sbt" },
+	callback = function()
+		require("metals").initialize_or_attach(metals_config)
+	end,
+	group = nvim_metals_group,
 })
