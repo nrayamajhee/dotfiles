fb37e172736371fa972f9a4af1259bc129443f58
nrayamajhee <nishan@rayamajhee.com>
2023-05-25 16:11:49 -0700

    Swap goto typedef/dec hotkeys

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -30,11 +30,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 		vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, opts)
 		vim.keymap.set({ "n", "v" }, "<leader>cc", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "<leader>ss", vim.lsp.buf.signature_help, opts)
-		vim.keymap.set("n", "<leader>kk", vim.lsp.buf.hover, opts)
-
-		--vastigial
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', '<leader>ss', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<leader>kk', vim.lsp.buf.hover, opts)
+    
+    --vastigial
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.declaration, opts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 
 		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
@@ -42,7 +42,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "<space>wl", function()
 			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 		end, opts)
-		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, opts)
 		vim.keymap.set("n", "<space>f", function()
 			vim.lsp.buf.format({ async = true })
 		end, opts)
