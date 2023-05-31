819226565bc203561cf25ab950285f70c67a1002
nrayamajhee <nishan@rayamajhee.com>
2023-05-30 21:23:49 -0700

    Some LSP tweaks

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -26,26 +26,28 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local opts = { buffer = ev.buf }
 		--vital
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		-- let trouble hande it
-		-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, opts)
 		vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, opts)
 		vim.keymap.set({ "n", "v" }, "<leader>cc", vim.lsp.buf.code_action, opts)
 		vim.keymap.set('n', '<leader>ss', vim.lsp.buf.signature_help, opts)
-		vim.keymap.set('n', '<leader>kk', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', '<leader>hh', vim.lsp.buf.hover, opts)
+		-- let trouble hande it
+		-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     
-    --vastigial
+    --notusedoften
 		vim.keymap.set("n", "<space>D", vim.lsp.buf.declaration, opts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 
+    --vastigial
 		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
 		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
 		vim.keymap.set("n", "<space>wl", function()
 			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 		end, opts)
-		vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "<space>f", function()
-			vim.lsp.buf.format({ async = true })
-		end, opts)
+		-- let formatter hande it
+		-- vim.keymap.set("n", "<space>f", function()
+		-- 	vim.lsp.buf.format({ async = true })
+		-- end, opts)
 	end,
 })
 
