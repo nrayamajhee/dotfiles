e0f583c019d1155ca6016e5f9f4726b9f2b13de8
nrayamajhee <nishan@rayamajhee.com>
2023-11-06 15:52:44 -0800

    Upgrade telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
 	"numToStr/Comment.nvim",
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
+		tag = "0.1.4",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
